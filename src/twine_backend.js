67678f2f5e6e5e8bdebc028d9aefa979afd0f2a8
Emmanuel Gaillot <emmanuel.gaillot@gmail.com>
2016-03-28 21:36:13 +0200

    Switch to express

--- src/twine_backend.js
+++ src/twine_backend.js
@@ -1,15 +1,21 @@
-var fs = require("fs"),
-    http = require("http");
-
 var createServer = function (fileName) {
-  return http.createServer(function (request, response) {
-    if (request.method == 'POST') {
-      request.pipe(fs.createWriteStream('data/' + fileName));
-      response.end();
-    } else {
-      fs.createReadStream('data/' + fileName).pipe(response);
-    }
+  var fs = require("fs"),
+      http = require("http"),
+
+      express = require("express"),
+
+      app = express();
+
+  app.post('/', function (request, response) {
+    request.pipe(fs.createWriteStream('data/' + fileName));
+    response.end();
   });
+
+  app.get('/', function (request, response) {
+      fs.createReadStream('data/' + fileName).pipe(response);
+  });
+
+  return http.createServer(app);
 };
 
 module.exports.createServer = createServer;
